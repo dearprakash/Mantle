1c5fc07d805608f5abe65c2b5b008a535d5d5882
Prakash Rajendran <prakash@applifiedideas.com>
2019-11-28 14:43:43 +0100

    add Package.swift

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "Mantle",
+    platforms: [
+        .iOS(.v8),
+        .tvOS(.v9),
+        .macOS(.v10),
+        .watchOS(.v2)
+    ],
+    products: [
+        .library(name: "Mantle", targets: ["Mantle"])
+    ],
+    dependencies: [ ],
+    targets: [
+        .target(
+            name: "Mantle",
+            dependencies: [],
+            path: "Mantle"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
